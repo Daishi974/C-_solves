e2a0d1c2ecb98e27412080c9cb878adba0f4a700
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2023-08-14 21:18:06 +0600

    Domino_piling.cpp

---
+++ Domino_piling.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/problemset/problem/50/A
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int n,p,i,t,c=0;
+ int m[100];
+ cin>>n>>t;
+
+ int d = (n*t) / 2;
+
+
+
+ cout<<d;
+
+}
