8694c9e28208d7c59c1a82e838de0e1295b03dc2
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2023-08-18 23:50:06 +0600

    To My Critics.cpp

---
+++ To My Critics.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/contest/1850/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    
+int i,j,c=0;
+int n,m,t,l;
+int arr[100], arr1[100];
+int p=0,k=0,u=0,q=0;
+string s;
+
+
+cin>>t;
+
+while(t--)
+{
+  cin>>n>>m>>l;
+
+ if(n+m>=10 || m+l>=10 || n+l>=10)
+ {
+  cout<<"YES"<<endl;
+ }
+ else{
+  cout<<"NO"<<endl;
+ }
+  
+ 
+}
+
+
+}
